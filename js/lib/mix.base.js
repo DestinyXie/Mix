8e3b071a91bff50260fffacb994b8f7eff9ab3cd
DestinyXie <xber1986@gmail.com>
2014-02-10 18:14:36 +0800

    add another template function

--- js/lib/mix.base.js
+++ js/lib/mix.base.js
@@ -375,6 +375,25 @@
                 return ret || "";
             });
         },
+        /*一款可以在模板里面写js代码的模板工具*/
+        compiTpl2: function(tpl, data) {
+            var re = /<%([^%>]+)?%>/g;
+            var reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
+            var code =  'var r = [];\n';
+            var cursor = 0;
+            var add =  function(line, js) {
+                js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+                (code += line !='' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+                return add;
+            }
+            while(match = re.exec(tpl)) {
+                add(tpl.slice(cursor, match.index))(match[1], true);
+                cursor = match.index + match[0].length;
+            }
+            add(tpl.substr(cursor, tpl.length - cursor));
+            code += 'return r.join("");';
+            return new Function(code.replace(/[\r\t\n]/g, '')).apply(data);
+        },
         /*错误log*/
         logErr: function(err, fnName, useAlert) {
             var useAlert = !! useAlert,
